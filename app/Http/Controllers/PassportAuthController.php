472f1da5dfc172307c26fcd8943e401e87327e1a
Abhay Dixit <abhaydixit336@gmail.com>
2022-05-05 16:26:45 +0530

    init

--- app/Http/Controllers/PassportAuthController.php
+++ app/Http/Controllers/PassportAuthController.php
@@ -7,8 +7,45 @@ use App\Models\User;
 
 class PassportAuthController extends Controller
 {
-    /**
-     * Registration
+   /**
+     * @OA\Post(
+     * path="/api/register",
+     * operationId="Register",
+     * tags={"User"},
+     * summary="User Register",
+     * description="User Register here",
+     *     @OA\RequestBody(
+     *         @OA\JsonContent(),
+     *         @OA\MediaType(
+     *            mediaType="multipart/form-data",
+     *            @OA\Schema(
+     *               type="object",
+     *               required={"name","email", "password", "password_confirmation"},
+     *               @OA\Property(property="name", type="text"),
+     *               @OA\Property(property="email", type="text"),
+     *               @OA\Property(property="password", type="password"),
+     *               @OA\Property(property="password_confirmation", type="password")
+     *            ),
+     *        ),
+     *    ),
+     *      @OA\Response(
+     *          response=201,
+     *          description="Register Successfully",
+     *          @OA\JsonContent()
+     *       ),
+     *      @OA\Response(
+     *          response=200,
+     *          description="Register Successfully",
+     *          @OA\JsonContent()
+     *       ),
+     *      @OA\Response(
+     *          response=422,
+     *          description="Unprocessable Entity",
+     *          @OA\JsonContent()
+     *       ),
+     *      @OA\Response(response=400, description="Bad request"),
+     *      @OA\Response(response=404, description="Resource Not Found"),
+     * )
      */
     public function register(Request $request)
     {
@@ -29,8 +66,43 @@ class PassportAuthController extends Controller
         return response()->json(['token' => $token], 200);
     }
 
-    /**
-     * Login
+   /**
+     * @OA\Post(
+     * path="/api/login",
+     * operationId="authLogin",
+     * tags={"User"},
+     * summary="User Login",
+     * description="Login User Here",
+     *     @OA\RequestBody(
+     *         @OA\JsonContent(),
+     *         @OA\MediaType(
+     *            mediaType="multipart/form-data",
+     *            @OA\Schema(
+     *               type="object",
+     *               required={"email", "password"},
+     *               @OA\Property(property="email", type="email"),
+     *               @OA\Property(property="password", type="password")
+     *            ),
+     *        ),
+     *    ),
+     *      @OA\Response(
+     *          response=201,
+     *          description="Login Successfully",
+     *          @OA\JsonContent()
+     *       ),
+     *      @OA\Response(
+     *          response=200,
+     *          description="Login Successfully",
+     *          @OA\JsonContent()
+     *       ),
+     *      @OA\Response(
+     *          response=422,
+     *          description="Unprocessable Entity",
+     *          @OA\JsonContent()
+     *       ),
+     *      @OA\Response(response=400, description="Bad request"),
+     *      @OA\Response(response=404, description="Resource Not Found"),
+     * )
      */
     public function login(Request $request)
     {
@@ -46,10 +118,36 @@ class PassportAuthController extends Controller
             return response()->json(['error' => 'Unauthorised'], 401);
         }
     }
-    public function logout (Request $request) {
+   /**
+     * @OA\Post(
+     *      path="/api/logout",
+     *      operationId="logoutUser",
+     *      tags={"User"},
+     *      summary="Logout",
+     *      @OA\Response(
+     *          response=200,
+     *          description="Successful operation",
+     *          @OA\JsonContent()
+     *       ),
+     *      @OA\Response(
+     *          response=401,
+     *          description="Unauthenticated",
+     *      ),
+     *      @OA\Response(
+     *          response=403,
+     *          description="Forbidden"
+     *      )
+     *     )
+     */
+    public function logout(Request $request)
+    {
         $accessToken = auth()->user()->token();
-        $token= $request->user()->tokens->find($accessToken);
-        $token->revoke();
-        return response(['message' => 'You have been successfully logged out.'], 200);
+        $token = $request->user()->tokens->find($accessToken);
+
+        if ($token->revoke()) {
+            return response(['message' => 'You have been successfully logged out.'], 200);
+        } else {
+            return response()->json(['error' => 'Unauthorised'], 401);
         }
+    }
 }
--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -1,72 +1,217 @@
 <?php
+
 namespace App\Http\Controllers;
+
 use Illuminate\Http\Request;
 use App\Models\Post;
+
 class PostController extends Controller
 {
+   /**
+     * @OA\Get(
+     *      path="/api/posts",
+     *      operationId="getPostList",
+     *      tags={"Post"},
+     *      summary="Get list of post",
+     *      description="Returns list of projects",
+     *      @OA\Response(
+     *          response=200,
+     *          description="Successful operation",
+     *          @OA\JsonContent()
+     *       ),
+     *      @OA\Response(
+     *          response=401,
+     *          description="Unauthenticated",
+     *      ),
+     *      @OA\Response(
+     *          response=403,
+     *          description="Forbidden"
+     *      )
+     *     )
+     */
     public function index()
     {
         $posts = auth()->user()->posts;
- 
+        // $posts=Post::all();
+
         return response()->json([
             'success' => true,
             'data' => $posts
         ]);
     }
- 
-    public function show($id)
-    {
-        $post = auth()->user()->posts()->find($id);
- 
-        if (!$post) {
-            return response()->json([
-                'success' => false,
-                'message' => 'Post not found '
-            ], 400);
-        }
- 
-        return response()->json([
-            'success' => true,
-            'data' => $post->toArray()
-        ], 400);
-    }
- 
+
+   /**
+     * @OA\Post(
+     *      path="/api/posts",
+     *      operationId="createPost",
+     *      tags={"Post"},
+     *      summary="Create new post",
+     *      description="Returns post data",
+     *      @OA\RequestBody(
+     *          required=true,
+     *          @OA\JsonContent(),
+     *          @OA\MediaType(
+     *            mediaType="multipart/form-data",
+     *            @OA\Schema(
+     *               type="object",
+     *               required={"title", "description"},
+     *               @OA\Property(property="title", type="text"),
+     *               @OA\Property(property="description", type="longText")
+     *            ),
+     *        ),
+     *      ),
+     *      @OA\Response(
+     *          response=201,
+     *          description="Successful operation",
+     *          @OA\JsonContent()
+     *       ),
+     *      @OA\Response(
+     *          response=400,
+     *          description="Bad Request"
+     *      ),
+     *      @OA\Response(
+     *          response=401,
+     *          description="Unauthenticated",
+     *      ),
+     *      @OA\Response(
+     *          response=403,
+     *          description="Forbidden"
+     *      )
+     * )
+     */
+
+    
     public function store(Request $request)
     {
         $this->validate($request, [
             'title' => 'required',
             'description' => 'required'
         ]);
- 
+        
         $post = new Post();
         $post->title = $request->title;
         $post->description = $request->description;
- 
+        
         if (auth()->user()->posts()->save($post))
-            return response()->json([
-                'success' => true,
-                'data' => $post->toArray()
-            ]);
+        return response()->json([
+            'success' => true,
+            'data' => $post->toArray()
+        ]);
         else
+        return response()->json([
+            'success' => false,
+            'message' => 'Post not added'
+        ], 500);
+    }
+   /**
+     * @OA\Get(
+     *      path="/api/posts/{id}",
+     *      operationId="getPostById",
+     *      tags={"Post"},
+     *      summary="Get post information",
+     *      description="Returns post data",
+     *      @OA\Parameter(
+     *          name="id",
+     *          description="Post id",
+     *          required=true,
+     *          in="path",
+     *          @OA\Schema(
+     *              type="integer"
+     *          )
+     *      ),
+     *      @OA\Response(
+     *          response=200,
+     *          description="Successful operation",
+     *          @OA\JsonContent()
+     *       ),
+     *      @OA\Response(
+     *          response=400,
+     *          description="Bad Request"
+     *      ),
+     *      @OA\Response(
+     *          response=401,
+     *          description="Unauthenticated",
+     *      ),
+     *      @OA\Response(
+     *          response=403,
+     *          description="Forbidden"
+     *      )
+     * )
+     */
+    public function show($id)
+    {
+        $post = auth()->user()->posts()->find($id);
+
+        if (!$post) {
             return response()->json([
                 'success' => false,
-                'message' => 'Post not added'
-            ], 500);
+                'message' => 'Post not found '
+            ], 400);
+        }
+
+        return response()->json([
+            'success' => true,
+            'data' => $post->toArray()
+        ], 400);
     }
- 
+
+   /**
+     * @OA\Put(
+     *      path="/api/posts/{id}",
+     *      operationId="updatePost",
+     *      tags={"Post"},
+     *      summary="Update existing post",
+     *      description="Returns updated project data",
+     *      @OA\Parameter(
+     *          name="id",
+     *          description="Project id",
+     *          required=true,
+     *          in="path",
+     *          @OA\Schema(
+     *              type="integer"
+     *          )
+     *      ),
+     *      @OA\RequestBody(
+     *          required=true,
+     *          @OA\JsonContent()
+     *      ),
+     *      @OA\Response(
+     *          response=202,
+     *          description="Successful operation",
+     *          @OA\JsonContent()
+     *       ),
+     *      @OA\Response(
+     *          response=400,
+     *          description="Bad Request"
+     *      ),
+     *      @OA\Response(
+     *          response=401,
+     *          description="Unauthenticated",
+     *      ),
+     *      @OA\Response(
+     *          response=403,
+     *          description="Forbidden"
+     *      ),
+     *      @OA\Response(
+     *          response=404,
+     *          description="Resource Not Found"
+     *      )
+     * )
+     */
+
     public function update(Request $request, $id)
     {
         $post = auth()->user()->posts()->find($id);
- 
+
         if (!$post) {
             return response()->json([
                 'success' => false,
                 'message' => 'Post not found'
             ], 400);
         }
- 
+
         $updated = $post->fill($request->all())->save();
- 
+
         if ($updated)
             return response()->json([
                 'success' => true
@@ -77,18 +222,53 @@ class PostController extends Controller
                 'message' => 'Post can not be updated'
             ], 500);
     }
- 
+
+   /**
+     * @OA\Delete(
+     *      path="/api/posts/{id}",
+     *      operationId="deletePost",
+     *      tags={"Post"},
+     *      summary="Delete existing post",
+     *      description="Deletes a post and returns no content",
+     *      @OA\Parameter(
+     *          name="id",
+     *          description="Post id",
+     *          required=true,
+     *          in="path",
+     *          @OA\Schema(
+     *              type="integer"
+     *          )
+     *      ),
+     *      @OA\Response(
+     *          response=204,
+     *          description="Successful operation",
+     *          @OA\JsonContent()
+     *       ),
+     *      @OA\Response(
+     *          response=401,
+     *          description="Unauthenticated",
+     *      ),
+     *      @OA\Response(
+     *          response=403,
+     *          description="Forbidden"
+     *      ),
+     *      @OA\Response(
+     *          response=404,
+     *          description="Resource Not Found"
+     *      )
+     * )
+     */
     public function destroy($id)
     {
         $post = auth()->user()->posts()->find($id);
- 
+
         if (!$post) {
             return response()->json([
                 'success' => false,
                 'message' => 'Post not found'
             ], 400);
         }
- 
+
         if ($post->delete()) {
             return response()->json([
                 'success' => true
